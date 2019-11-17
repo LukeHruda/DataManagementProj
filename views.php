a0a1398157c605032cda1263641c65000c1df929
Cool Hand Luke <49134640+LukeHruda@users.noreply.github.com>
2019-11-17 13:53:08 -0500

    Updated Php, added css

--- views.php
+++ views.php
@@ -2,18 +2,18 @@
 <html>
 <head>
 	<title>Here is some data</title>
+	<link href="views.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-<table>
-	<tr>
-		<th>ID</th>
-		<th>Model</th>
-		<th>ID</th>
-		<th>Fname</th>
-		<th>ID</th>
-		<th>Fname</th>
-	</tr>
+<table class ="t1" summary="Shows all the Trains by ID and Model. With which engineer and conductor serves on them.">
+<caption>Trains in service with associated Conductor and Engineer</caption>
+	<thead>
+	<tr><th>ID</th><th>Model</th><th>ID</th><th>Fname</th><th>ID</th><th>Fname</th></tr>
+	</thead>
+	<tfoot>
+	<tr><th colspan="6">Final Project Group 1</th></tr>
+	</tfoot>
 	<?php
 	$dbhost = "localhost";
 	$dbuser = "root";
@@ -27,10 +27,11 @@ INNER JOIN conductors ON train.trainID = conductors.TrainID)";
 	
 	if($result-> num_rows >0)
 	{
+		echo "<tbody>";
 		while($row = $result-> fetch_assoc())
 		{
 			echo "<tr>";
-			echo "<td>". $row["trainID"] ."</td>";
+			echo "<th>". $row["trainID"] ."</th>";
 			echo "<td>". $row["Model"] ."</td>";
 			echo "<td>". $row["conID"] ."</td>";
 			echo "<td>". $row["conFname"] ."</td>";
@@ -38,6 +39,7 @@ INNER JOIN conductors ON train.trainID = conductors.TrainID)";
 			echo "<td>". $row["engFname"] ."</td>";
 			echo "</tr>";
 		}
+		echo "</tbody>";
 		echo "</table>";
 	}
 	$conn-> close();
