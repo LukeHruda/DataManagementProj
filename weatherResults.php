1844c1ab7c343f9104b388ac48707a62f0f8c8b8
thedansilva <43654534+thedansilva@users.noreply.github.com>
2019-11-29 12:22:41 -0500

    rounding

--- weatherResults.php
+++ weatherResults.php
@@ -16,7 +16,7 @@
 	}
 	$response = json_decode(file_get_contents('http://api.openweathermap.org/data/2.5/weather?q='.$location.',Ca&appid=524b9e33ddf5c6c8ee78d8a03411de9f'), true);
 	$kelvin = 273.16; 
-	$weather = $response["main"]["temp"] - $kelvin;
+	$weather = number_format(($response["main"]["temp"] - $kelvin), 2);
 	if($weather > 0 && $weather < 1) 
 			$weather = 1;
 	echo "<h4>The weather in ".$response["name"]." is currently ".$weather." degrees. Conditions: ".$response['weather'][0]['main'].".</h4>";
